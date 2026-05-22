29adad9a29c3d15402bc280d11ebd9a7724b0f46
Carlosss543 <charles.herr54@gmail.com>
2026-05-23 01:19:19 +0200

    chagement de paramètres d'entrainement

--- model_training_code/training_parameters.py
+++ model_training_code/training_parameters.py
@@ -4,11 +4,11 @@ num_classes = 100
 resize_size = 256
 crop_size = 224
 
-num_epochs = 200
-batch_size = 128
+num_epochs = 300
+batch_size = 256
 
 lr = 0.001
-lr_warmup_epochs = 10
+lr_warmup_epochs = 5
 lr_warmup_decay = 0.033
 
 weight_decay = 0.01
@@ -17,11 +17,11 @@ mixup_alpha = 0.2
 cutmix_alpha = 1.0
 clip_grad_norm = 1.0
 
-model_type = "custom_vit_s_16"
-use_bias = True
+model_type = "custom_vit_t_16"
+use_bias = False
 bias_only = None
 bias_topk = None
 bias_score_threshold = None
 
-checkpoints_interval = 50
-folder_number = 15
+checkpoints_interval = 100
+folder_number = 10
